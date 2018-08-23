f6ca128ad80e604f535e7911c1e9ce6a96d1a2b7
Dominik Soboń <dominik.sobon@gmail.com>
2018-08-23 17:20:25 +0200

    Change program.js

--- program.js
+++ program.js
@@ -1,14 +1,21 @@
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else {
+        switch (instruction) {
+			case '/exit' :
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/node v' :
+				console.log(process.versions['node']);
+				break;
+			case '/sys lang':
+				console.log(process.env.LANG);
+				break;
+			default:	
             process.stderr.write('Wrong instruction!');
         }
     }
